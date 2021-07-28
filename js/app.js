e9a85962e88845211d08af70da23044f9c3732a2
avblackwell <blackwellalex0@gmail.com>
2021-07-28 06:42:22 -0400

    select html elements and add event listeners

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const bill = document.getElementById('inp-bill');
+const tipBtns = document.querySelectorAll('.tip');
+const tipCustom = document.getElementById('inp-tip');
+const people = document.getElementById('inp-people');
+const errorMsg = document.querySelector('.error-msg');
+const results = document.querySelectorAll('.value');
+const resetBtn = document.querySelector('.reset');
+
+
+bill.addEventListener('input', setBillValue);
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+tipCustom.addEventListener('input', setTipCustomValue);
+people.addEventListener('input', setPeopleValue);
+resetBtn.addEventListener('click', reset);
