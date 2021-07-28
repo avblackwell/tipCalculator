332a358f329571575296d4b3f9edcd7745f6a099
avblackwell <blackwellalex0@gmail.com>
2021-07-28 07:03:29 -0400

    set default value for variables + functions to validate float/int using regex

--- js/app.js
+++ js/app.js
@@ -14,3 +14,17 @@ tipBtns.forEach(btn => {
 tipCustom.addEventListener('input', setTipCustomValue);
 people.addEventListener('input', setPeopleValue);
 resetBtn.addEventListener('click', reset);
+
+let billValue = 0.0; //default value
+let tipValue = 0.15; //default value -> 15% button is active
+let peopleValue = 1; 
+
+function validateFloat(s){
+    var rgx = /^[0-9]*\.?[0-9]*$/;
+    return s.match(rgx);
+}
+
+function validateInt(s){
+    var rgx = /^[0-9]*$/;
+    return s.match(rgx);
+}
